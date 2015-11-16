c0b9497442bd893859592ccd6fb5f0af921ec6f1
jsstrn <jessternrays@gmail.com>
2015-11-16 15:30:15 +0800

    Update files

---
+++ app/meme.js
@@ -0,0 +1,4 @@
+var canvas = document.getElementById('canvas')
+var content = canvas.getContext('2d')
+canvas.width = Math.min(500, window.innerWidth - 100)
+canvas.height = Math.min(500, window.innerWidth - 100)
