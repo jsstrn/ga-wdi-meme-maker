dd99a0a0c8d9c304406e0f47816e8e3f195e6f5e
jsstrn <jessternrays@gmail.com>
2015-11-16 16:21:13 +0800

    Update files

--- app/meme.js
+++ app/meme.js
@@ -1,4 +1,11 @@
 var canvas = document.getElementById('canvas')
-var content = canvas.getContext('2d')
+var context = canvas.getContext('2d')
 canvas.width = Math.min(500, window.innerWidth - 100)
 canvas.height = Math.min(500, window.innerWidth - 100)
+
+var img = new Image()
+img.src = 'app/img/doge.jpg'
+img.onload = function () {
+  console.log('Image has loaded')
+  context.drawImage(img, 0, 0, canvas.width, canvas.height)
+}
